aced0a1357e72f672f0fd2dfc691928e775952ad
Marton Borlay <borlaym@gmail.com>
2018-10-05 21:02:21 +0200

    planet positions

--- src/createPlanets.ts
+++ src/createPlanets.ts
@@ -6,21 +6,28 @@ export default function createPlanets() {
 	const mercuryTexture = new THREE.TextureLoader().load('textures/mercury.jpg');
 	const mercuryMaterial = new THREE.MeshBasicMaterial({ color: 0xffffff, map: mercuryTexture });
 	const mercury = new THREE.Mesh(mercuryGeometry, mercuryMaterial);
+	mercury.position.x = 57.9 * 4
 
 	const venusGeometry = new THREE.SphereGeometry(12.104, 32, 32);
 	const venusTexture = new THREE.TextureLoader().load('textures/venus.png');
 	const venusMaterial = new THREE.MeshBasicMaterial({ color: 0xffffff, map: venusTexture });
 	const venus = new THREE.Mesh(venusGeometry, venusMaterial);
+	venus.position.x = 108.2 * 4
+
 
 	const earthGeometry = new THREE.SphereGeometry(12.756, 32, 32);
 	const earthTexture = new THREE.TextureLoader().load('textures/earth.jpg');
 	const earthMaterial = new THREE.MeshBasicMaterial({ color: 0xffffff, map: earthTexture });
 	const earth = new THREE.Mesh(earthGeometry, earthMaterial);
+	earth.position.x = 149.6 * 4
+
 
 	const marsGeometry = new THREE.SphereGeometry(6.792, 32, 32);
 	const marsTexture = new THREE.TextureLoader().load('textures/mars.jpg');
 	const marsMaterial = new THREE.MeshBasicMaterial({ color: 0xffffff, map: marsTexture });
 	const mars = new THREE.Mesh(marsGeometry, marsMaterial);
+	mars.position.x = 227.9 * 4
+
 
 
 
--- src/createStarBackground.ts
+++ src/createStarBackground.ts
@@ -8,7 +8,7 @@ export default function createStarBackground() {
 	for (let i = 0; i < 10000; i++) {
 		let distance = 0
 		const star = new THREE.Vector3();
-		while (distance < 200) {
+		while (distance < 500) {
 			star.x = THREE.Math.randFloatSpread(1000);
 			star.y = THREE.Math.randFloatSpread(1000);
 			star.z = THREE.Math.randFloatSpread(1000);
