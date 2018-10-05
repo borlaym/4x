f55ec808f8f87c3ea49e654e702afe9ab395463e
Marton Borlay <borlaym@gmail.com>
2018-10-05 20:58:59 +0200

    add inner planets

--- src/createPlanets.ts
+++ src/createPlanets.ts
@@ -1,12 +1,28 @@
 import * as THREE from "three";
 
-const sphereGeometry = new THREE.SphereGeometry(1, 32, 32);
-
-
 export default function createPlanets() {
+
+	const mercuryGeometry = new THREE.SphereGeometry(4.879, 32, 32);
+	const mercuryTexture = new THREE.TextureLoader().load('textures/mercury.jpg');
+	const mercuryMaterial = new THREE.MeshBasicMaterial({ color: 0xffffff, map: mercuryTexture });
+	const mercury = new THREE.Mesh(mercuryGeometry, mercuryMaterial);
+
+	const venusGeometry = new THREE.SphereGeometry(12.104, 32, 32);
+	const venusTexture = new THREE.TextureLoader().load('textures/venus.png');
+	const venusMaterial = new THREE.MeshBasicMaterial({ color: 0xffffff, map: venusTexture });
+	const venus = new THREE.Mesh(venusGeometry, venusMaterial);
+
+	const earthGeometry = new THREE.SphereGeometry(12.756, 32, 32);
 	const earthTexture = new THREE.TextureLoader().load('textures/earth.jpg');
 	const earthMaterial = new THREE.MeshBasicMaterial({ color: 0xffffff, map: earthTexture });
-	const earth = new THREE.Mesh(sphereGeometry, earthMaterial);
+	const earth = new THREE.Mesh(earthGeometry, earthMaterial);
 
-	return [earth]
+	const marsGeometry = new THREE.SphereGeometry(6.792, 32, 32);
+	const marsTexture = new THREE.TextureLoader().load('textures/mars.jpg');
+	const marsMaterial = new THREE.MeshBasicMaterial({ color: 0xffffff, map: marsTexture });
+	const mars = new THREE.Mesh(marsGeometry, marsMaterial);
+
+
+
+	return [mercury, venus, earth, mars]
 }
--- src/index.tsx
+++ src/index.tsx
@@ -6,8 +6,8 @@ import createPlanets from 'createPlanets';
 const scene = new THREE.Scene();
 
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-camera.position.y = 10;
-camera.position.z = 4;
+camera.position.y = 200;
+camera.position.z = 200;
 camera.rotation.x = -Math.PI / 2
 
 const renderer = new THREE.WebGLRenderer();
